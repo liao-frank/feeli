3bf3ab73b2c8a03854305d81ed48038c329c29d7
Stephanie Pang <spang@andrew.cmu.edu>
2017-09-08 23:13:00 -0400

    Removed ToneAnalyzer duplicate

--- js/ToneAnalyzer.js
+++ js/ToneAnalyzer.js
@@ -1,8 +1,22 @@
 
 class Tone_API {
 
-	constructor(api_url, ) {
+	constructor(api_url, username, password) {
 		this.response = JSON.stringify({});
-
+		this.api_url  = api_url;
+		this.username = username;
+		this.password = password;
 	}
+
+	request(text, callback, errorCallback){
+		// call API
+		// format response
+		// callback
+		// errorCallback
+	}
+
+	formatResponse() {
+		// format response into dict
+	}
+
 }
