25bb0124c6ef780a5d3e59f644153a62ea817582
recs7168 <recs7168@gmail.com>
2017-09-09 17:36:27 -0400

    footer and ticks

--- js/foo.js
+++ js/foo.js
@@ -2,27 +2,6 @@ class Foo {
 
 	constructor() {}
 
-	handleResponse(tonesDict) {
-		if (Object.keys(tonesDict).length != 0){
-			var joyHeight = tonesDict["emotion_tone"]["Joy"];
-			var angerHeight = tonesDict["emotion_tone"]["Anger"];
-			var disgustHeight = tonesDict["emotion_tone"]["Disgust"];
-			var fearHeight = tonesDict["emotion_tone"]["Fear"];
-			var sadnessHeight = tonesDict["emotion_tone"]["Sadness"];
-
-			this.setHeight('.joy.bar', joyHeight);
-			this.setHeight('.anger.bar', angerHeight);
-			this.setHeight('.disgust.bar', disgustHeight);
-			this.setHeight('.fear.bar', fearHeight);
-			this.setHeight('.sadness.bar', sadnessHeight);
-		}
-	};
-
-	switchTab(selector) {
-		$('.page').removeClass('active');
-		$(`.page${selector}`).addClass('active');
-	};
-
 	setHeight(selector, value) {
 		let start = 5;
 		let end = 100;
@@ -35,17 +14,55 @@ class Foo {
 		$('.bar').css('height', `${start}px`);
 	};
 
-	// Takes in a big 5 class e.g. ".neuroticism"
-	setScale(selector, value) {
-		let start = -2.2;
-		let end = 97.4;
-		let range = end - start;
-
-		$(`${selector}.scale-space .tick`).css('left', `${range * value + start}%`)
-	};
+	
 
 	resetScales() {
 		$('.tick').css('left', '');
+	};
+
+	handleResponse(tonesDict) {
+		function setHeight(selector, value) {
+			let start = 5;
+			let end = 125;
+			let range = end - start;
+			$(selector).css('height', `${value * range + start}px`);
+		};
+
+		// Takes in a big 5 class e.g. ".neuroticism"
+		function setScale(selector, value) {
+			let start = -2.2;
+			let end = 97.4;
+			let range = end - start;
+
+			$(`${selector}.scale-space .tick`).css('left', `${range * value + start}%`)
+		};
+
+		if (Object.keys(tonesDict).length != 0) {
+			var joyValue = tonesDict["emotion_tone"]["Joy"];
+			var angerValue = tonesDict["emotion_tone"]["Anger"];
+			var disgustValue = tonesDict["emotion_tone"]["Disgust"];
+			var fearValue = tonesDict["emotion_tone"]["Fear"];
+			var sadnessValue = tonesDict["emotion_tone"]["Sadness"];
+
+			setHeight('.joy.bar', joyValue);
+			setHeight('.anger.bar', angerValue);
+			setHeight('.disgust.bar', disgustValue);
+			setHeight('.fear.bar', fearValue);
+			setHeight('.sadness.bar', sadnessValue);
+
+			let social = tonesDict['social_tone'];
+
+			setScale('.openness', social['Openness']);
+			setScale('.conscientiousness', social['Conscientiousness']);
+			setScale('.extraversion', social['Extraversion']);
+			setScale('.agreeableness', social['Agreeableness']);
+			setScale('.neuroticism', social['Emotional Range']);
+		}
+	};
+
+	switchTab(selector) {
+		$('.page').removeClass('active');
+		$(`.page${selector}`).addClass('active');
 	};
 
 	displayErrorMessage(error) {
